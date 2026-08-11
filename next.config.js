2cdd9051ec4abc43d1ef03c05d5a1c8f506eff62
Ivan Correa <ivan@ekivibes.com>
2026-08-11 23:32:30 +0000

    fix: eliminar redirects permanentes de productos Ninos en next.config.js

--- next.config.js
+++ next.config.js
@@ -13,30 +13,7 @@ const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
  */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:country/products/chaleco-airbag-vh-ni%C3%B1os",
-        destination: "/:country/products/chaleco-airbag-vh-juvenil-adulto",
-        permanent: true,
-      },
-      {
-        source: "/:country/products/chaleco-airbag-mlv3-h-ni%C3%B1os",
-        destination: "/:country/products/chaleco-airbag-mlv3-h-juvenil-adulto",
-        permanent: true,
-      },
-      {
-        source: "/:country/products/chaleco-airbag-vh-ninos",
-        destination: "/:country/products/chaleco-airbag-vh-juvenil-adulto",
-        permanent: true,
-      },
-      {
-        source: "/:country/products/chaleco-airbag-mlv3-h-ninos",
-        destination: "/:country/products/chaleco-airbag-mlv3-h-juvenil-adulto",
-        permanent: true,
-      },
-    ]
-  },
+
   logging: {
     fetches: {
       fullUrl: true,
